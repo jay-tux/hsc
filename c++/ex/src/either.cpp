d06c46e79392bce4599989ea31a022847a41d87b
jay-tux <sys.jonas@yahoo.com>
2021-06-24 09:18:25 +0200

    Conditionally inherit from Show if template type is Show

--- c++/ex/src/either.cpp
+++ c++/ex/src/either.cpp
@@ -4,24 +4,20 @@
 using namespace hscpp;
 
 int main() {
-  std::function<Either<hsInt, hsString>(hsInt, hsInt)> saveDiv = [](hsInt a, hsInt b) {
-    return b == 0 ? Either<hsInt, hsString>(hsString::fromString("Divide by 0")) :
-                    Either<hsInt, hsString>(hsInt((int)a / (int)b));
+  std::function<Either<hsChar, cppString>(hsInt)> nthCharacter = [](hsInt v) {
+    return (int)v < 26 ? Either<hsChar, cppString>::right(hsChar('a' + (int)v)) :
+      Either<hsChar, cppString>::left(cppString("Character should be < 26"));
   };
 
-  std::function<hsInt(hsInt)> writer = [](hsInt a) {
-    std::cout << "Result: " << show(a) << std::endl;
-    return a;
-  };
-  std::function<Either<hsInt, hsString>(hsInt)> div12 = curry(saveDiv, hsInt(12));
+  std::function<hsString(hsChar)> conv = [](hsChar chr) { return show(chr); };
 
-  auto chain = [div12, writer](hsInt a) {
-    std::cout << "Dividing 12 by " << show(a) << std::endl;
-    Either<hsInt, hsString> res = Either<hsInt, hsString>(a).bind(div12).fmap(writer);
-    if(res.isError) std::cout << res.value.err << std::endl;
+  auto chain = [nthCharacter, conv](hsInt a) {
+    std::cout << "Getting n-th character: " << show(a) << std::endl;
+    std::cout << Either<hsInt, cppString>::pure(a)
+      .bind(nthCharacter).fmap(conv).show() << std::endl;;
   };
 
-  hsInt num1(6), num2(18), num3(0);
+  hsInt num1(6), num2(0), num3(33);
 
   chain(num1);
   chain(num2);
